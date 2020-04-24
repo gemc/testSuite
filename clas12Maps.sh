6e0a6804636615adabef8b6a5910ae0f2f3e3ebe
maureeungaro <maureeungaro@gmail.com>
2020-04-24 10:49:52 -0400

    more general scripts deal with different maps and interpolation

---
+++ clas12Maps.sh
@@ -0,0 +1,78 @@
+#!/bin/zsh
+
+if [ "$#" -ne 2 ]; then
+	echo " "       >&2
+	echo " Usage:" >&2
+	echo " "       >&2
+	echo  " $0 interpolation field"  >&2
+	echo " "       >&2
+	echo " interpolation: <None> or <Linear>"  >&2
+	echo " field: <solenoid> or <torus>"       >&2
+	echo " "       >&2
+	exit 1
+fi
+
+interpolation=$1
+field=$2
+
+echo
+echo Interpolation: $interpolation
+echo Field: $field
+echo
+
+# units in mm
+
+npoints=100
+
+minX=1
+maxX=5000
+minY=1
+maxY=5000
+minZ=-3000
+maxZ=3000
+
+if [[ $field == "torus" ]]; then
+	minX=1
+	maxX=5000
+	minY=1
+	maxY=5000
+	minZ=-3000
+	maxZ=3000
+	echo "torus field map"
+fi
+
+
+
+xvalues=(${(f)"$(jot -r $npoints $minX $maxX)"})
+yvalues=(${(f)"$(jot -r $npoints $minY $maxY)"})
+zvalues=(${(f)"$(jot -r $npoints $minZ $maxZ)"})
+
+fileNamePoints=solenoidPoints$interpolation
+
+rm -f $fileNamePoints.txt ; touch $fileNamePoints.txt
+
+for i in {1..$npoints}
+do
+	# decimal points
+	decimal=(${(f)"$(jot -r 3 0 1000)"})
+	echo "("$xvalues[$i]"."$decimal[1]", "$yvalues[$i]"."$decimal[2]", "$zvalues[$i]"."$decimal[3]")mm" >> $fileNamePoints.txt
+done
+
+vertex=(${(f)"$(cat $fileNamePoints.txt)"})
+
+fileNameG=gemc$fileNamePoints
+fileNameL=gemcLog$fileNamePoints
+
+rm -f $fileNameG.txt ; touch $fileNameG.txt
+rm -f $fileNameL.txt ; touch $fileNameL.txt
+
+# first output the options in the file just to make sure. Do not run gemc
+./runGemc.sh $vertex[$i] $interpolation $field no | grep -A7 "> Magnetic Field" >> $fileNameL.txt
+
+for i in {1..$npoints}
+do
+	echo vertex $vertex[$i] $interpolation $field
+	./runGemc.sh $vertex[$i] $interpolation $field yes | grep -A1 "Track position in magnetic field map," >> $fileNameG.txt
+done
+
+
--- runGemc.sh
+++ runGemc.sh
@@ -1,11 +1,35 @@
 #!/bin/csh
 
-setenv vertex  "$1"
-set option='-HALL_FIELD=clas12-newSolenoid -FIELD_VERBOSITY=99 -USE_GUI=0 -N=1'
 
-echo script vertex $vertex
-
-
-gemc $option -BEAM_V="$vertex" -BEAM_P="e-, 1*GeV, 90*deg 0*deg"
+setenv vertex        "$1"
+setenv interpolation $2
+setenv field         $3
+setenv run           $4
 
 
+
+set option = '-HALL_FIELD=clas12-newSolenoid -FIELD_VERBOSITY=99 -USE_GUI=0 -N=1'
+set fprop  = 'clas12-newSolenoid, 1*mm, G4ClassicalRK4, linear'
+
+if ($interpolation == "none") then
+	set fprop  = 'clas12-newSolenoid, 1*mm, G4ClassicalRK4, none'
+endif
+
+if ($field == "torus") then
+	echo fieldmap: torus
+	set option = '-HALL_FIELD=TorusSymmetric -FIELD_VERBOSITY=99 -USE_GUI=0 -N=1'
+	set fprop  = 'TorusSymmetric, 2*mm, G4ClassicalRK4, linear'
+
+	if ($interpolation == "none") then
+		set fprop  = 'TorusSymmetric, 2*mm, G4ClassicalRK4, none'
+	endif
+endif
+
+
+
+if ( $run == "no") then
+	echo script vertex $vertex interpolation: $interpolation $field option $option field properties: "$fprop"
+	gemc $option -BEAM_V="$vertex" -BEAM_P="e-, 1*GeV, 90*deg 0*deg" -FIELD_PROPERTIES="$fprop"
+else
+	gemc $option -BEAM_V="$vertex" -BEAM_P="e-, 1*GeV, 90*deg 0*deg" -FIELD_PROPERTIES="$fprop"
+endif
